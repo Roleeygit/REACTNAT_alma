27ae2f0532538c181c7f8fcc7df297a16712eb42
Roleeygit <juhaszroli2001@gmail.com>
2023-04-18 09:41:30 +0200

    apple is working and style added

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Name: Juhász Roland
+ * Group: Szoft II N
+ * Date: 2023-04-18
+ */
 
-export default function App() {
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+function calculatePercentage(total, count) 
+{
+  return ((count / total) * 100).toFixed(2);
+}
+
+export default function App() 
+{
+  const [firstClassApples, setFirstClassApples] = useState(0);
+  const [secondClassApples, setSecondClassApples] = useState(0);
+  const totalApples = firstClassApples + secondClassApples;
+  const firstClassPercentage = calculatePercentage(totalApples, firstClassApples);
+  const secondClassPercentage = calculatePercentage(totalApples, secondClassApples);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>almaosztaly</Text>
+      <Text style={styles.subheader}>készítette: Juhász Roland (Szoft II N)</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Number of first-class apples"
+        keyboardType="numeric"
+        onChangeText={(value) => setFirstClassApples(parseInt(value))}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Number of second-class apples"
+        keyboardType="numeric"
+        onChangeText={(value) => setSecondClassApples(parseInt(value))}
+      />
+      <Text style={styles.result}>Összes kapott almák száma: {totalApples}</Text>
+      <Text style={styles.result}>Első osztályú almák százalékban: {firstClassPercentage}%</Text>
+      <Text style={styles.result}>Másodosztályú almák százalékban: {secondClassPercentage}%</Text>
     </View>
   );
 }
@@ -16,5 +50,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subheader: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  result: {
+    marginVertical: 10,
+    fontSize: 16,
   },
 });
